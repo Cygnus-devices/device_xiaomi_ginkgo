f9b0cd16dc042ba0efb443817eac2d7cc31a5c03
erwinabs <erwinabubakarsidik@gmail.com>
2022-03-24 00:51:45 +0000

    ginkgo: init Cygnus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_ginkgo.mk
+    $(LOCAL_DIR)/cygnus_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_ginkgo-user \
-    arrow_ginkgo-userdebug \
-    arrow_ginkgo-eng
+    cygnus_ginkgo-user \
+    cygnus_ginkgo-userdebug \
+    cygnus_ginkgo-eng
